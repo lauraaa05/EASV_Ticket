cc65224ad404142d0fe35ae349abf2f31b059898
Emre <aliemreuzunoglu@gmail.com>
2025-03-17 10:51:24 +0100

    A small chagne

--- src/main/java/dk/easv/Main.java
+++ src/main/java/dk/easv/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/EASV TICKET - LogIn.fxml"));
         primaryStage.setTitle("Login Screen");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
