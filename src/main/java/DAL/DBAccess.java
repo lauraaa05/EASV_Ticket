8c202d3b9871c61eb934c1a556723bde82a36bc7
katja981 <katstr01@easv365.dk>
2025-03-17 11:31:43 +0100

    I created a database connection class and DAL

---
+++ src/main/java/DAL/DBAccess.java
@@ -0,0 +1,20 @@
+package DAL;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
+public class DBAccess {
+    public Connection DBConnection() throws SQLServerException {
+        SQLServerDataSource ds;
+        ds = new SQLServerDataSource();
+        ds.setDatabaseName("EASVTicket_SixTix_Int");
+        ds.setUser("CSe2024b_e_15");
+        ds.setPassword("CSe2024bE15!24");
+        ds.setServerName("EASV-DB4");
+        ds.setPortNumber(1433);
+        ds.setTrustServerCertificate(true);
+        return ds.getConnection();
+    }
+}
