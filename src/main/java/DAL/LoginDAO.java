f5b3a3bed27819bf9532fdeb02cc4ecfc8eff602
katja981 <katstr01@easv365.dk>
2025-03-20 14:44:27 +0100

    I've made it so we can login with a username and password from the database. I've created the classes LoginCheck and LoginDAO

---
+++ src/main/java/DAL/LoginDAO.java
@@ -0,0 +1,35 @@
+package DAL;
+
+import be.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDAO {
+
+    private DBAccess con = new DBAccess();
+
+    public User getUserByUserName(String username) {
+        User user = null;
+
+        try (Connection c = con.DBConnection();) {
+            String sql = "SELECT Username, Password, Role FROM LoginInfo WHERE Username = ?";
+            PreparedStatement stmt = c.prepareStatement(sql);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                String dbUsername = rs.getString("Username");
+                String dbPassword = rs.getString("Password");
+                String dbRole = rs.getString("Role");
+
+                user = new User(dbUsername, dbPassword, dbRole);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
--- src/main/java/bll/UserManagement.java
+++ src/main/java/bll/UserManagement.java
@@ -1,27 +1,14 @@
 package bll;
 
 import be.User;
-import DAL.UserDAO;
 
 public class UserManagement {
 
-    private final UserDAO userDAO = new UserDAO();
-
-    public void createAdmin(String username, String password) {
-        User admin = new User(username, password, "Admin");
-        userDAO.addUser(admin);
+    public User createAdmin(String Username, String Password) {
+        return new User(Username, Password, "Admin");
     }
 
-    public void createEventCoordinator(String username, String password) {
-        User coordinator = new User(username, password, "Event Coordinator");
-        userDAO.addUser(coordinator);
-    }
-
-    public User getUser(String username) {
-        return userDAO.getUserByUsername(username);
-    }
-
-    public void deleteUser(String username) {
-        userDAO.deleteUser(username);
+    public User createEventCoordinator(String Username, String Password) {
+        return new User(Username, Password, "Coordinator");
     }
 }
